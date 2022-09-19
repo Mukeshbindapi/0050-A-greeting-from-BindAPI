4dbae9b178cf1d7ae140fc0095bc2ebad4946e61
Mukeshbindapi <mukesh.k@bindapi.com>
2022-09-19 12:55:43 +0530

    first commit

--- index.php
+++ index.php
@@ -1,3 +1,60 @@
+<?php
+    if(isset($_POST['submit']))
+    {
+        $first = $_POST['first_name'];
+        $last =  $_POST['last_name'];
+        $mobile = $_POST['mobile'];
+
+        echo "<h2>Success</h2>"; 
+    }
+?>
+
+<?php
+    $servername = "localhost";
+    $dbname = "dev_8";
+    $username = "root";
+    $password = "";
+?>
+
+<?php
+  $form = new create($servername, $dbname, $username, $password);
+  if(isset($_POST['submit']))
+  {
+      $first_name = $_POST['first_name'];
+      $last_name = $_POST['last_name'];
+      $mobile = $_POST['mobile'];
+
+      $form->adddata($first_name, $last_name, $mobile);
+
+  }
+?>
+
+<?php
+    class create
+    {
+        public $conxn;
+        
+        public function __construct($servername, $dbname, $username, $password)
+        {
+            $this->conxn = new PDO("mysql:host=$servername;dbname=$dbname",$username,$password);
+        }
+
+        public function adddata($first_name, $last_name, $mobile)
+        {
+            $query = "INSERT INTO `create_form` VALUES (NULL, ?, ?, ?)";
+            $statment = $this->conxn->prepare($query);
+            $statment->execute([$first_name, $last_name, $mobile]);
+        
+        
+        }
+    }
+?>
+
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,16 +65,18 @@
 </head>
 <body>
     <form  method="post">
-        <input type="text"name="first_name"placeholder="First_Name">
-        <input type="text"name="second_name"placeholder="Second_Name">
-        <button type="submit"name="submit">send</button>
+        <br><input type="text"name="first_name"placeholder="First_Name"></br>
+        <br><input type="text"name="last_name"placeholder="last_name"></br>
+        <br><input type="text"name="mobile"placeholder="mobile"></br>
+        <br><button type="submit"name="submit">send</button></br>
         <h2>
             <?php
                 if(isset($_POST['submit']))
                 {
                     $first_name = $_POST['first_name'];
-                    $second_name = $_POST['second_name'];
-                    echo "Hello $first_name $second_name! Greetings from BindAPI";
+                    $last_name = $_POST['last_name'];
+                    $mobile = $_POST['mobile'];
+                    echo "Hello $first_name $last_name $mobile! Greetings from BindAPI";
                 }
             ?>
         </h2>
